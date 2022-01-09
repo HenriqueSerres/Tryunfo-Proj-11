aede3c7910a3bc952527579bdc957f6968d72251
HenriqueSerres <henriqueserres@gmail.com>
2022-01-09 20:22:17 -0300

    requisito 5 e 6

--- src/App.js
+++ src/App.js
@@ -15,49 +15,103 @@ class App extends React.Component {
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
+      armazenador: [],
     };
   }
 
-  handleChange = ({ target }) => {
-    const { name } = target;
-    const value = target.type === 'checkbox'
-      ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    });
+  ableToSave = () => {
+    const max = 90;
+    const maxSum = 210;
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare } = this.state;
+    if (
+      cardName === ''
+      || cardDescription === ''
+      || cardImage === ''
+      || cardRare === ''
+      || Number(cardAttr1) > max || Number(cardAttr1) < 0
+      || Number(cardAttr2) > max || Number(cardAttr2) < 0
+      || Number(cardAttr3) > max || Number(cardAttr3) < 0
+      || (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) > maxSum)
+    ) {
+      this.setState({ isSaveButtonDisabled: true });
+    } else {
+      this.setState({ isSaveButtonDisabled: false });
+    }
   }
 
-  render() {
-    const { cardName, cardDescription, cardAttr1,
-      cardAttr2, cardAttr3, cardImage, cardRare,
-      cardTrunfo } = this.state;
-    return (
-      <div>
-        <h1>Tryunfo</h1>
-        <Form
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-          onInputChange={ this.handleChange }
-        />
-        <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-        />
-      </div>
-    );
-  }
+    handleChange = ({ target }) => {
+      const { name } = target;
+      const value = target.type === 'checkbox'
+        ? target.checked : target.value;
+      this.setState({
+        [name]: value,
+      }, () => this.ableToSave());
+    }
+
+    onSaveButtonClick = (event) => {
+      event.preventDefault();
+      const { cardName, cardDescription, cardAttr1,
+        cardAttr2, cardAttr3, cardImage, cardRare,
+        cardTrunfo, armazenador } = this.state;
+      this.setState({
+        cardName: '',
+        cardDescription: '',
+        cardAttr1: '0',
+        cardAttr2: '0',
+        cardAttr3: '0',
+        cardImage: '',
+        cardRare: 'normal',
+        cardTrunfo: false,
+        isSaveButtonDisabled: true,
+        armazenador: armazenador.concat([{
+          cardName,
+          cardDescription,
+          cardAttr1,
+          cardAttr2,
+          cardAttr3,
+          cardImage,
+          cardRare,
+          cardTrunfo,
+        }]),
+      });
+    }
+
+    render() {
+      const { cardName, cardDescription, cardAttr1,
+        cardAttr2, cardAttr3, cardImage, cardRare,
+        cardTrunfo, isSaveButtonDisabled } = this.state;
+      return (
+        <div>
+          <h1>Tryunfo</h1>
+          <Form
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+            onInputChange={ this.handleChange }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            handleChange={ this.handleChange }
+            onSaveButtonClick={ this.onSaveButtonClick }
+          />
+          <Card
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+          />
+        </div>
+      );
+    }
 }
 
 export default App;
